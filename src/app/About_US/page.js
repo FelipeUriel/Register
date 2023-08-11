4fa304d72797370dbd59df962d8ad4dc6f3c4da9
Uriel Rodríguez <urielochoardz@gmail.com>
2023-08-11 07:54:02 -0600

    Salvar la materia

---
+++ src/app/About_US/page.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div>
+        <h1>  SOY FELIPE URIEL</h1>
+        </div>
+    )
+}
---
+++ src/app/Profile/page.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div>
+        <h1>  SOY PROFILE </h1>
+        <h2>  SOY FELIPE URIEL </h2>
+        </div>
+    )
+}
